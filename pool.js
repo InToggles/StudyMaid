9a4a4cc269199e63545617ed1ee81ef95f2ca020
InToggles <intogglegamings@gmail.com>
2022-04-01 01:47:56 +0800

    make it better

--- pool.js
+++ pool.js
@@ -2,24 +2,20 @@ const util = require('util');
 const mysql = require('mysql');
 /** Connection to the database.**/
 const pool = mysql.createPool({
-    host: 'us-cdbr-east-05.cleardb.net',
-    user: 'b3096bc9773b7e',
-    password: '3a6acd58',
-    database: 'heroku_2a0b36d47694a4d',
-
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: "heroku_2a0b36d47694a4d",
 });
 
 pool.getConnection((error, connection) => {
-    if(error) {
-        console.error("There was an error connecting to Database.", error); 
-    }
+    if(error) 
+        console.error("There was an error connecting to Database.", error);
     
-    if(connection) {
-        console.log("Successfully connected to the Database!")
+    if(connection)
+    console.log("Successfully connected to the Database!")
         connection.release();
     return;
-    }
-
 });
 
 pool.query = util.promisify(pool.query);
