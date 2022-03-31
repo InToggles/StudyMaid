e258f51821d74ddf40f0632f7f84fa4c9753995c
InToggles <intogglegamings@gmail.com>
2022-04-01 01:33:50 +0800

    make it better

--- pool.js
+++ pool.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    database: "StudyMaid",
+    database: process.env.DATABASE,
 });
 
 pool.getConnection((error, connection) => {
