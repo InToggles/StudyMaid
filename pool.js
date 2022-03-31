48099e73fa2c737b5adce123e129455f17b7d2aa
InToggles <intogglegamings@gmail.com>
2022-04-01 02:06:09 +0800

    make it better

--- pool.js
+++ pool.js
@@ -3,9 +3,9 @@ const mysql = require('mysql');
 console.log(process.env.DATABASE_HOST2, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD)
 /** Connection to the database.**/
 const pool = mysql.createPool({
-    host: process.env.DATABASE_HOST2,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
+    host: 'us-cdbr-east-05.cleardb.net',
+    user: 'b3096bc9773b7e',
+    password: '3a6acd58',
     database: "heroku_2a0b36d47694a4d",
 });
 
